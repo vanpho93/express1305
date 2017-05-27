f4d179f49b208c4077cc48148bfb46cf13894cf6
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-27 10:43:50 +0700

    ejs

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/a', require('./controller/routeA'));
 
 app.get('/say/:name/:age', (req, res) => {
     const { name, age } = req.params;
     res.send(`<h1>Xin chao ${name}, ${age} tuoi!</h1>`);
 });
+
+app.get('/trangchu', (req, res) => res.render('home'));
 
 app.get('/tinh/:pt/:soa/:sob', require('./controller/routeTinh'));
 
