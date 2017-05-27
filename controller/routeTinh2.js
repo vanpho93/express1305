ddd5263a7179adc4488f5ea7727a94f4df2b9906
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-27 11:28:50 +0700

    static

--- controller/routeTinh2.js
+++ controller/routeTinh2.js
@@ -3,7 +3,7 @@ const PhepTinh = require('../model/PhepTinh');
 const tinhController = (req, res) => {
     const { pt, soa, sob } = req.params;
     const myPt = new PhepTinh(pt, +soa, +sob);
-    res.render('tinh', { myPt: myPt })
+    res.render('tinh', { myPt })
 };
 
 module.exports = tinhController;
