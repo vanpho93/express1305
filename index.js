91dd1d434f3c64c371de2ccccc1ebfc1d0e6f613
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-27 11:50:20 +0700

    het buoi sang

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser'); //1
+
+const parser = bodyParser.urlencoded({ extended: false }); //2
 
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/a', require('./controller/routeA'));
+
+app.post('/xuly', parser, (req, res) => { //3
+    const { txtUsername, txtPassword } = req.body;
+    res.send('THANH_CONG: ' + txtUsername + ' ::: ' + txtPassword);
+});
 
 app.get('/say/:name/:age', (req, res) => {
     const { name, age } = req.params;
@@ -21,8 +28,6 @@ app.get('/tinh2/:pt/:soa/:sob', require('./controller/routeTinh2'));
 
 app.get('/show', (req, res) => res.render('show'));
 
-app.post('/xuly', (req, res) => {
 
-});
 
 app.listen(3000);
