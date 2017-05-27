946ce0b9f1c74aaa574f0c3ad0219578ac12a735
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-27 09:49:42 +0700

    name and age

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const app = express();
 
 app.get('/a', require('./controller/routeA'));
 
+app.get('/say/:name/:age', (req, res) => {
+    const { name, age } = req.params;
+    res.send(`Xin chao ${name}, ${age} tuoi!`);
+});
+
+//localhost:3000/say/PHO
+
 app.listen(3000);
